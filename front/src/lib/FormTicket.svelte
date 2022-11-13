ddf7ad16f9421434b4ca46670f62858b1be76309
Lumexio <trotroyu@gmail.com>
2022-11-12 18:14:32 -0600

    feat: Cards form skeleton

---
+++ front/src/lib/FormTicket.svelte
@@ -0,0 +1,57 @@
+<script>
+  	let questions = [
+		{ id: 1, text: `Pal norte` },
+		{ id: 2, text: `X next` },
+		{ id: 3, text: `Rihanna
+      ` }
+	];
+
+	let selected;
+
+	let answer = '';
+</script>
+<style>
+  .card{
+    background-color: aliceblue;
+    border-radius: 1em;
+    margin-bottom: 2em;
+  }
+  input{
+    border-radius: 3px;
+        font-size: 1.5rem;
+   margin: 5px;
+   
+  }
+  select{
+      border-radius: 3px;
+        font-size: 1.5rem;
+   margin: 5px;
+   width: 97%;
+  }
+  button{
+border: transparent;
+    font-size: 1.2rem;
+  }
+  button:hover{
+    background-color: goldenrod;
+    color: black;
+    border: transparent;
+
+  }
+</style>
+<div class="card">
+  <select  value={selected} on:change="{() => answer = ''}">
+		{#each questions as question}
+			<option  value={question}>
+				{question.text}
+			</option>
+		{/each}
+	</select><br>
+  
+  <input type="number" placeholder="Amount to buy"><br>
+  <div>
+<button>
+  Buy
+</button>
+</div>
+</div>
